91c6dc0b282daee709545e79550bcf425213f951
rcoles14 <90427464+rcoles14@users.noreply.github.com>
2022-01-09 18:56:40 -0700

    Add project files.

---
+++ MyFirstWebApp/wwwroot/js/fanmail.js
@@ -0,0 +1,12 @@
+﻿////document.getElementById("btnSend").addEventListener("click", function () {
+////    alert("From: " + document.getElementById("txtFrom").value + ", Subject: "
+////        + document.getElementById("txtSub").value
+////        + ", Message: " + document.getElementById("txtMsg").value)
+////})
+
+$("#btnSend").click(function () {
+    alert("From " + $("#txtFrom").val() + $("#txtSub").val() + $("#txtMsg").val())
+
+
+    $("#picganderson").fadeOut("slow")
+})
